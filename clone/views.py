c288f3a4d1e928402a525ae3d81c1906419543b0
JKimani77 <jkkimani77@gmail.com>
2020-03-08 23:20:47 +0300

    created email template files and a view function to activate users

--- clone/views.py
+++ clone/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
+from .tokens import account_activation_token
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+
 from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from .models import Image, Profile, Comment
@@ -19,7 +23,7 @@ def index(request):
     images = Image.get_images()
     return render(request, 'home.html', {"images":images})
 
-def signup(request):
+def signingup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -28,7 +32,7 @@ def signup(request):
             user.save()
             current = get_current_site(request)
             subject = 'Activate your iNsTa'
-            message = render_to_string('email/email.html', {'user': user, 'domain': current.domain,})
+            message = render_to_string('email/email.html', {'user': user, 'domain': current.domain, 'uid': urlsafe_base64_encode(force_bytes(user.pk)), 'token': account_activation_token.make_token(user),})
             
             user.email_user(subject, message)
             return redirect('home')
@@ -36,3 +40,18 @@ def signup(request):
             form = SignUpForm()
             return render(request, 'signup.html', {'form': form})
 
+
+def activation(request, uidb64, token):
+  try:
+    user = User.objects.get()
+  except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+    user = None
+
+  if user is not None and account_activation_token.check_token(user, token):
+    user.is_active = True
+    user.profile.email_confirmed = True
+    user.save()
+    login(request, user)
+    return redirect('home')
+  else:
+    return render(request, 'accountinvalid.html')
